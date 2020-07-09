49ae9f74c0ca3622336239c79219850267e7a2a6
Hoàng Dương <hoangduongit1997@gmail.com>
2020-07-09 08:47:50 +0700

    remove

--- lib/app/data/model/intro_slider.dart
+++
@@ -1,9 +0,0 @@
-class Intro_slider {
-  final String title;
-  final String subtitle;
-  final String icon;
-  Intro_slider({this.title, this.subtitle, this.icon});
-  String get titles => title;
-  String get subtitles => subtitle;
-  String get icons => icon;
-}
